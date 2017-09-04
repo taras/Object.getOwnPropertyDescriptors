530abf87f7bcc10f6646bfdc6e39401e765ff89d
Taras Mankovski <tarasm@gmail.com>
2017-09-04 17:46:48 +0530

    Removed the default its not a default export

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,3 @@
-export default function getOwnPropertyDescriptors<T>(
+export function getOwnPropertyDescriptors<T>(
   o: T
 ): { [P in keyof T]: TypedPropertyDescriptor<T[P]> } & { [x: string]: PropertyDescriptor };
